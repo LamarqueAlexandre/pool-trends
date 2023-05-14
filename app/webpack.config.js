39fa71e48ec1e2fb6eb5d6cc7950ccc0ce126a22
Alexandre Lamarque <lamarque.alexandre7@gmail.com>
2023-05-14 18:02:23 +0200

    Integration registration form

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -78,4 +78,9 @@ Encore
     //.autoProvidejQuery()
 ;
 
-module.exports = Encore.getWebpackConfig();
+const config = Encore.getWebpackConfig();
+config.watchOptions = {
+    poll: true,
+}
+
+module.exports = config;
