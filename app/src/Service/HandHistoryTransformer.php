41964e5f00bff85f5e1fff2f2032696cfa724e29
Alexandre Lamarque <lamarque.alexandre7@gmail.com>
2023-06-08 12:03:18 +0200

    Write parser logic

--- app/src/Service/HandHistoryTransformer.php
+++ app/src/Service/HandHistoryTransformer.php
@@ -5,10 +5,58 @@ namespace App\Service;
 class HandHistoryTransformer
 {
     private array $allHands = [];
+    private string $idHandHistory;
 
     public function convertHandHistoryToArray(string $fileToTransform)
     {
-        dump($fileToTransform);
-        exit;
+        $handle = fopen($fileToTransform, 'r');
+
+        if ($handle) {
+            while (($line = fgets($handle)) !== false) {
+                // Récupération de l'identifiant de la HandHistory
+                $this->getHandHistoryId($line);
+                $this->addHandHistory();
+                $this->addNoLimitHoldem($line);
+                $this->addDateHandHistory($line);
+            }
+
+            fclose($handle);
+
+            dump($this->allHands);
+            exit;
+
+            return $this->allHands;
+        } else {
+            dump('Erreur lors de l\'ouverture du fichier');
+            exit;
+        }
+    }
+
+    public function getHandHistoryId(string $line)
+    {
+        if (preg_match('/#(\d+-\d+-\d+)/', $line, $matches)) {
+            $this->idHandHistory = $matches[1];
+        }
+    }
+
+    public function addHandHistory()
+    {
+        if (!isset($this->allHands[$this->idHandHistory])) {
+            $this->allHands[$this->idHandHistory] = [];
+        }
+    }
+
+    public function addNoLimitHoldem(string $line)
+    {
+        if (preg_match('/\((\d+\.\d+€\/\d+\.\d+€)\)/', $line, $matches)) {
+            $this->allHands[$this->idHandHistory]["No-Limit Holdem"] = $matches[1];
+        }
+    }
+
+    public function addDateHandHistory(string $line)
+    {
+        if (preg_match('/(\d{4}\/\d{2}\/\d{2} \d{2}:\d{2}:\d{2})/', $line, $matches)) {
+            $this->allHands[$this->idHandHistory]["Date"] = $matches[1];
+        }
     }
 }
